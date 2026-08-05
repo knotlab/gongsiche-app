3dcc61e48ed12c3b891550725483eba16c3c0fa1
knotlab <skgoldwise@gmail.com>
2026-08-05 22:54:13 +0900

    PWA 갱신

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* 공시체 계산기 PWA — 오프라인 캐시. 본업(계산·작업)은 지하에서도 떠야 한다 */
-const VER = 'gsc-ece1c79040';
-const FILES = ["./css/app.css","./icon-192.png","./icon-512.png","./icon.svg","./index.html","./js/app.bundle.js","./manifest.json"];
+const VER = 'gsc-df1f1c3865';
+const FILES = ["./css/app.css","./icon-192.png","./icon-512.png","./icon.svg","./index.html","./js/app.bundle.js","./manifest.json","./robots.txt"];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(VER).then((c) => c.addAll(FILES)).then(() => self.skipWaiting()));
 });
